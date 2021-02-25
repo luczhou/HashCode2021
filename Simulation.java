956800d463e4ff744a749b933ad895167303bb27
Joy Wang <49984055+joyjwang@users.noreply.github.com>
2021-02-25 16:20:42 -0500

    Add files via upload

--- Simulation.java
+++ Simulation.java
@@ -1,5 +1,6 @@
 package traffic;
 
+import java.io.File;
 import java.util.*;
 
 public class Simulation {
@@ -10,6 +11,41 @@ public class Simulation {
 	Map<Integer, Intersection> intersections = new HashMap<Integer, Intersection>();
 	
 	public static void main(String[] args){
+		File text = new File("C:/Users/Public/Joy/HashCode/a.txt");
+		Scanner input = new Scanner(text);
 		
+		int numTime = input.nextInt();
+		int numIntersections = input.nextInt();
+		int numStreets = input.nextInt();
+		int numCars = input.nextInt();
+		
+		input.next();
+		
+		
+		int start, end, seconds;
+		String street;
+		for (int i = 0; i < numStreets; i++) {
+			start = input.nextInt();
+			end = input.nextInt();
+			street = input.next();
+			seconds = input.nextInt();
+			
+			System.out.println(start);
+			System.out.println(end);
+			System.out.println(street);
+			System.out.println(seconds);
+		}
+
+		int numStreetInPath;
+		String street_path;
+		for (int i = 0; i < numCars; i++) {
+			numStreetInPath = input.nextInt();
+			System.out.println(numStreetInPath);
+			
+			for (int j = 0; j < numStreetInPath; j++) {
+				street_path = input.next();
+				System.out.println(street_path);
+			}
+		}
 	}
 }
