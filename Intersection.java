6eacaedac75c4d0b1d953d113411d045791fef17
Lucinda Zhou <4885420+luczhou@users.noreply.github.com>
2021-02-25 15:19:04 -0500

    Add files via upload

--- Intersection.java
+++ Intersection.java
@@ -3,12 +3,14 @@ package traffic;
 import java.util.*;
 
 public class Intersection {
+	Set<Car> cars;
 	Map<String, Street> streets;
 	int num;
 	
 	public Intersection(int num){
 		this.num = num;
 		streets = new HashMap<>();
+		cars = new HashSet<Car>();
 	}
 	
 	public void addStreet(Street street){
--- Simulation.java
+++ Simulation.java
@@ -1,11 +1,15 @@
 package traffic;
 
+import java.util.*;
+
 public class Simulation {
 	int totalTime;
 	int destinationPoints;
+	ArrayList<Car> cars = new ArrayList<Car>();
+	Set<Street> streets = new HashSet<Street>();
+	Set<Intersection> intersections = new HashSet<Intersection>();
 	
-	public Simulation(int totalTime, int destinationPoints){
-		this.totalTime = totalTime;
-		this.destinationPoints = destinationPoints;
+	public static void main(String[] args){
+		
 	}
 }
